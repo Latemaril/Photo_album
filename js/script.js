e1bcc26aa0a89a242d0c3920a30aa1fa56309885
VladiSlave <sukhovey.vlad@gmail.com>
2022-12-19 22:01:24 +1000

    c_3.0

--- js/script.js
+++ js/script.js
@@ -1,63 +1,84 @@
-const dragItems = document.querySelectorAll(".js_item");
-const dropZones = document.querySelectorAll(".js_cell");
+const dragItems = document.querySelectorAll(".js_item")
+const dropZones = document.querySelectorAll(".js_cell")
+// const right = document.querySelector('.right');
 
-dragItems.forEach((dragItem) => {
-    dragItem.addEventListener("dragstart", handlerDragstart);
-    dragItem.addEventListener("dragend", handlerDragend);
-    dragItem.addEventListener("drag", handlerDrag);
+// right.addEventListener('drop', event => {
+//     console.log(event.target)
+//     event.preventDefault()
+//
+//     const dragFlag = event.dataTransfer.getData("js_item");
+//     const dragItem = document.querySelector(`[data-item="${dragFlag}"]`);
+//     const clone = dragItem.cloneNode(true);
+//     clone.id = "dick"
+//
+//     console.log(event.target.className)
+//     if (event.target.className.includes('js_cell')) {
+//         event.target.append(clone);
+//     } else {
+//         const parent = event.target.parentNode
+//         parent.removeChild(event.target)
+//         parent.append(clone)
+//     }
+// })
+
+dragItems.forEach((js_item) => {
+    js_item.addEventListener("dragstart", handlerDragstart)
+    js_item.addEventListener("dragend", handlerDragend)
+    js_item.addEventListener("drag", handlerDrag)
 
 })
 
 dropZones.forEach((js_cell) => {
-    js_cell.addEventListener("dragenter", handlerDragenter);
-    js_cell.addEventListener("dragleave", handlerDragleave);
-    js_cell.addEventListener("dragover", handlerDragover);
-    js_cell.addEventListener("drop", handlerDrop);
+    js_cell.addEventListener("dragenter", handlerDragenter)
+    js_cell.addEventListener("dragleave", handlerDragleave)
+    js_cell.addEventListener("dragover", handlerDragover)
+    js_cell.addEventListener("drop", handlerDrop)
 })
 
 function handlerDragstart(event) {
-    event.dataTransfer.setData("js_item", this.dataset.item);
+    event.dataTransfer.setData("js_item", this.dataset.item)
 
-    this.classList.add("js_item--active");
+    this.classList.add("js_item--active")
 }
 
 function handlerDragend(event) {
-    this.classList.remove("js_item--active");
-
+    console.log("Fuck")
+    this.classList.remove("js_item--active")
 }
 
 function handlerDrag(event) {}
 
 function handlerDragenter(event) {
-    event.preventDefault();
-    this.classList.add("js_cell--active");
+    event.preventDefault()
+    this.classList.add("js_cell--active")
 }
 
 function handlerDragleave(event) {
-    this.classList.remove("js_cell--active");
+    this.classList.remove("js_cell--active")
 }
 
 function handlerDragover(event) {
-    event.preventDefault();
+    event.preventDefault()
 }
 
 function handlerDrop(event) {
     this.classList.remove("js_cell--active")
 
-    if (document.getElementById("dick") !== null) {
-        document.getElementById("dick").remove();
-    }
+    console.log(event.target)
+    event.preventDefault()
 
-    const dragFlag = event.dataTransfer.getData("js_item");
-    const dragItem = document.querySelector(`[data-item="${dragFlag}"]`);
-
-    const clone = dragItem.cloneNode(true);
+    const dragFlag = event.dataTransfer.getData("js_item")
+    const dragItem = document.querySelector(`[data-item="${dragFlag}"]`)
+    dragItem.removeAttribute("draggable")
+    const clone = dragItem.cloneNode(true)
     clone.id = "dick"
-    this.append(clone);
-}
 
-document.querySelector('button').addEventListener('click', function() {
-    const c = document.querySelector('#cont').classList;
-    c.toggle('col-md-9');
-    document.querySelector('#side').classList.toggle('d-done');
-});
+    console.log(event.target.className)
+    if (event.target.className.includes('js_cell')) {
+        event.target.append(clone)
+    } else {
+        const parent = event.target.parentNode
+        parent.removeChild(event.target)
+        parent.append(clone)
+    }
+}
