82f4fcbd6fcfcdce4d84280637f9b5f01c0ae363
VladiSlave <sukhovey.vlad@gmail.com>
2022-12-20 17:57:07 +1000

    c_4.2

---
+++ js/album_buttons.js
@@ -0,0 +1,23 @@
+function pagesClear() {
+    const photos = document.querySelectorAll('.js_item--active');
+    console.log(photos)
+    photos.forEach(photo => {
+        photo.remove()
+    })
+
+}
+
+
+
+
+function createPDF() {
+    let pdf = document.getElementById("content")
+    let opt = {
+        margin: 1,
+        filename: 'html2pdf-converted.pdf',
+        image: { type: 'jpeg', quality: 1 },
+        html2canvas:  { scale: 2 },
+        jsPDF: { unit: 'in', format: 'a4', orientation: 'l' }
+    }
+    html2pdf().set(opt).from(pdf).save()
+}
